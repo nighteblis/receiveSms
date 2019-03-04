a0b882030b76f81321cd18d61f9a52b2f381ed13
lihao <nighteblis@hotmail.com>
2019-03-04 23:43:44 +0800

    add meta

--- headerinclude.php
+++ headerinclude.php
@@ -1,1 +1,6 @@
-<link rel="stylesheet" type="text/css" href="statics/sms.css?5">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="免费短信,免费收短信,短信注册,国际免费短信,免费收短信,国际短信,手机号免费注册" />
+<meta name="title" content="免费国际收短信-国际免费手机号注册" />
+<meta name="author" content="www.free-sms.cf" />
+<meta name="description" content="免费短信,免费收短信,短信注册,国际免费短信,免费收短信,国际短信,手机号免费注册" />
+<meta name="generator" content="http://www.free-sms.cf" />
